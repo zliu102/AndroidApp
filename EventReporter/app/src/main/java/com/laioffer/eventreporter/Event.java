66bbaf1f3cc41d9a046039759ec4aecb9d70713d
zliu102 <zliu102@hawk.iit.edu>
2017-12-07 14:22:54 -0600

    update

---
+++ EventReporter/app/src/main/java/com/laioffer/eventreporter/Event.java
@@ -0,0 +1,101 @@
+package com.laioffer.eventreporter;
+
+/**
+ * Created by liuziyu on 2017/11/7.
+ */
+
+public class Event {
+    /**
+     * All data for a event.
+     */
+    private String title;
+    private String address;
+    private String description;
+    private int like;
+    private String id;
+
+    public int getCommentNumber() {
+        return CommentNumber;
+    }
+
+    public void setCommentNumber(int commentNumber) {
+        CommentNumber = commentNumber;
+    }
+
+    private int CommentNumber;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImgUri() {
+        return imgUri;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
+    }
+
+    private long time;
+    private String username;
+    private String imgUri;
+
+    public Event() {}
+
+    /**
+     * Constructor
+     */
+    public Event(String title, String address, String description) {
+        this.title = title;
+        this.address = address;
+        this.description = description;
+    }
+
+    /**
+     * Getters for private attributes of Event class.
+     */
+    public String getTitle() { return this.title; }
+    public String getAddress() { return this.address; }
+    public String getDescription() { return this.description; }
+
+}
